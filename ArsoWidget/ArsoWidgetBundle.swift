c2c9a902cd75fafa449e8e61d9f4ad971dd6661b
DavidMismas <100374656+DavidMismas@users.noreply.github.com>
2026-03-20 07:17:37 +0100

    widgets working, graphical weather report

---
+++ ArsoWidget/ArsoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ArsoWidgetBundle.swift
+//  ArsoWidget
+//
+//  Created by David Mišmaš on 20. 3. 2026.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ArsoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ArsoWidget()
+    }
+}
